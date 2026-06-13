c2a6bda3f2b6431bd058cfc1f600f44daa56f47a
Tomás Leva <tomasgodoyleva@gmail.com>
2026-06-12 23:18:49 -0400

    delete obsolete code on remote repo

--- debug_import.py
+++
@@ -1,7 +0,0 @@
-import importlib
-import models.document
-
-print("Ruta del módulo:", models.document.__file__)
-print("Contenido disponible:", dir(models.document))
-importlib.reload(models.document)
-print("Después de recargar:", dir(models.document))
