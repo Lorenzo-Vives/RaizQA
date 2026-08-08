0904f06b53ae23fc024523a0fb11f7685a950966
ping-ern <ramirezg@estudiante.uc.cl>
2026-08-08 00:45:05 -0400

    -ADD: constants.py with file extensions being used

---
+++ core/constants.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------
+# Constantes y Utilidades
+# ----------------------------------------------------------------------
+TEXT_EXTENSIONS = {".txt", ".docx", ".pdf"}
+IMAGE_EXTENSIONS = {".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff"}
+ALLOWED_EXTENSIONS = tuple(TEXT_EXTENSIONS | IMAGE_EXTENSIONS)
+
+
+
